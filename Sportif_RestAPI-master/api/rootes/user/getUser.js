00445f9c4c637368911b595a4f1e3e46d3f0be9a
Znaki Ramy <rami.znaki222@gmail.com>
2019-03-28 13:10:22 +0100

    added getRessource route by id

--- Sportif_RestAPI-master/api/rootes/user/getUser.js
+++ Sportif_RestAPI-master/api/rootes/user/getUser.js
@@ -4,11 +4,21 @@ const { Router } = express;
 const router = Router();
 
 router.get('/getUser', function(req,res) {
-  // res.send("hello")
-  userModel.find(req.body).then(function(user){
+
+  userModel.find(req.body).then(function(users){
+    res.send(users);
+}).catch((err) => {
+  res.send('error')
+});
+});
+
+router.get('/getUser/:id', function(req,res) {
+  userModel.findOne(req.body.id).then(function(user){
     res.send(user);
 }).catch((err) => {
   res.send('error')
 });
 });
+
+
 export default router;
